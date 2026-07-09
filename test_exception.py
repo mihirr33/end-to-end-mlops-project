579d18b27d3a1c4f8f490dafa5cee323643d8a07
Mihirr Dobariya <mihirofficial33.edu@gmail.com>
2026-07-09 21:36:45 +0530

    Fix exception test

--- test_exception.py
+++ test_exception.py
@@ -1,7 +1,12 @@
-import sys
 from src.exception import CustomException
+import sys
 
 try:
     a = 10 / 0
+
 except Exception as e:
-    raise CustomException(e, sys)
+    error = CustomException(e, sys)
+
+    print(error)
+
+print("Exception Test Passed")
